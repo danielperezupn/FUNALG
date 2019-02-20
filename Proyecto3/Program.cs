e819ddf221226eeb9f457fa72bc929b5a9c4451e
111luverix <N00173986@upn.pe>
2019-02-20 11:46:30 -0500

    Revert "hola mundo 3"
    This reverts commit b9fcfb43ad5fbb502238178212b7e030106d80db.

--- Proyecto3/Program.cs
+++ Proyecto3/Program.cs
@@ -12,7 +12,6 @@ namespace Proyecto3
         {
             Console.WriteLine("Hola Mundo");
             Console.WriteLine("Hola Mundo 2");
-            Console.WriteLine("Hola Mundo 3");
             Console.ReadKey();
         }
     }
